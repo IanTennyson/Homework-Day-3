565b363d26c6577f6dcbfa6bb4fb6dd13ecbe202
Ian <iantennyson@gmail.com>
2017-03-08 17:41:05 +0000

    Day 3 Optional Homework

---
+++ optional_homework/start_point/my_functions.rb
@@ -0,0 +1,30 @@
+def add_array_lengths(array1, array2)
+  return array1.length + array2.length
+end
+
+def  sum_array(numbers)
+  total = 0
+  for num in numbers
+    total += num
+  end
+  return total
+end
+
+def find_item(array, item)
+  for house in array #in this example "house" stands for each element in the array
+    if house == item
+      return true
+    end
+  end
+else house == "Batman"
+  return false
+end
+
+
+
+
+def get_first_key(teachers_wallets)
+   return teachers_wallets.keys[0]
+end
+
+
